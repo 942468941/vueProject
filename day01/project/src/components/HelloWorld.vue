42b097b09ac7f3d35b5b5e3211818c15e2ee9c42
chenkaige <kaigechen@dessmann.com.cn>
2020-04-09 17:36:52 +0800

    初始化

---
+++ day01/project/src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hello">
+    <span v-once>执行一次性地插值:{{msg}}</span>
+      {{msg}}
+      <button @click="edit">更改</button>
+      <p>Using mustaches: {{ rawHtml }}</p>
+<p>Using v-html directive: <span v-html="rawHtml"></span></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'hhhh',
+      rawHtml:'<div style="color:red">777</div>'
+    }
+  },
+  created(){
+    this.msg = "kkkk"
+    console.log(this.msg)
+  },
+  methods:{
+    edit(){
+      this.msg = "ggg"
+    }
+  }
+  
+}
+</script>
+
+<style scoped>
+
+</style>
